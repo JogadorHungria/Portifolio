304c35b9568193d81bb01c47e31223425236f406
joilson <joilsonhungrianv@gmail.com>
2023-11-06 17:04:53 -0300

    fix: removing fold assets for not utilzation

--- src/componets/Main/certificates/index.tsx
+++ src/componets/Main/certificates/index.tsx
@@ -1,20 +1,14 @@
+import { certificateImages } from "../../../mock/certificates";
 import { Title } from "../../Titles";
 import { StyledCertificate } from "./style";
 
 export const Certificates = () => {
-  const teste = [
-    "src/assets/imagens/certificados/Full-stack.png",
-    "src/assets/imagens/certificados/certificate.png",
-    "src/assets/imagens/certificados/certificate-back-end.png",
-    "src/assets/imagens/certificados/certificado-maratona.png",
-  ];
-
   return (
     <div className="effectScrollMonitoring">
       <StyledCertificate>
         <Title text={"Certificados"} />
         <div>
-          {teste.map((image) => (
+          {certificateImages.map((image) => (
             <img src={image} alt="imagem" />
           ))}
         </div>
---
+++ src/mock/certificates.ts
@@ -0,0 +1,6 @@
+export const certificateImages = [
+  "/imagens/certificados/Full-stack.png",
+  "/imagens/certificados/certificate.png",
+  "/imagens/certificados/certificate-back-end.png",
+  "/imagens/certificados/certificado-maratona.png",
+];
